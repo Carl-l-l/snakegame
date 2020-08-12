58f955dcc3d9312ebeb3bcad3ddad42e911898f3
Alex <alexmoller02@hotmail.com>
2020-08-12 10:11:55 +0200

    Upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './header.js';
+import Body from './body.js';
+import Footer from './footer.js';
+
+
+function App() {
+  return (
+<div className="main-container">
+<Header></Header>
+<Body></Body>
+<Footer></Footer>
+</div>
+
+  );
+}
+export default App;
